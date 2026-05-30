b8435efb4a68b1ee7ece9b5ada34ab8975606bc2
Gabriel A. Ribeiro <gabrielaubinogame@gmail.com>
2026-05-30 01:55:17 -0300

    JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
 function profileInfoUpdate(infoProfileData){
+    // 
     const photo = document.getElementById('perfil.photo')
     photo.src = infoProfileData.photo
+    // 
+    const name = document.getElementById('profile.name')
+    name.innerText = infoProfileData.name
+
+    // 
+    const funcao = document.getElementById('profile.job')
+    funcao.innerText = infoProfileData.job
+    // 
+    const endereco = document.getElementById('profile.location')
+    endereco.innerText = infoProfileData.location
+    // 
+    const contato = document.getElementById('profile.phone')
+    contato.innerText = infoProfileData.phone
+    contato.href = `tel:${infoProfileData.phone}`
+    // 
+    const email = document.getElementById('profile.email')
+    email.innerText = infoProfileData.email
+    email.href = `mailto:${infoProfileData.email}`
+
 }
 
 
